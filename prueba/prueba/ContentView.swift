80e0ad7dda73f448e5c0ab67aff86fb2e3cea5d7
saxsolrac <saxsolrac@gmail.com>
2020-06-11 14:08:52 +0200

    primer proyecto
    proyecto prueba

---
+++ prueba/prueba/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba
+//
+//  Created by Carlos Antonio Calle Redondo on 11/06/2020.
+//  Copyright © 2020 saxsolrac. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prueba/pruebaTests/pruebaTests.swift
@@ -0,0 +1,34 @@
+//
+//  pruebaTests.swift
+//  pruebaTests
+//
+//  Created by Carlos Antonio Calle Redondo on 11/06/2020.
+//  Copyright © 2020 saxsolrac. All rights reserved.
+//
+
+import XCTest
+@testable import prueba
+
+class pruebaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
